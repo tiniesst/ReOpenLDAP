f81e1268dad79cc65d16609d26782b59dab691dc
Leo Yuriev <leo@yuriev.ru>
2015-01-21 18:45:50 +0300

    test: Teamcity integration, /bin/bash required.
    Change-Id: I8b1687600388d0b2568d37a20da7f1d6f150fb24

--- tests/scripts/defines.sh
+++ tests/scripts/defines.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/bash
 # $OpenLDAP$
 ## This work is part of OpenLDAP Software <http://www.openldap.org/>.
 ##
@@ -363,3 +363,9 @@ DDSOUT=$DATADIR/dds.out
 MEMBEROFOUT=$DATADIR/memberof.out
 MEMBEROFREFINTOUT=$DATADIR/memberof-refint.out
 SHTOOL="$SRCDIR/../build/shtool"
+
+function teamcity_msg {
+	if [ -n "${TEAMCITY_PROCESS_FLOW_ID}" ]; then
+		echo "##teamcity[$@]"
+	fi
+}
