104b2af292bac0e4e6c54d473702ccff5f737263
Leo Yuriev <leo@yuriev.ru>
2015-01-26 10:35:25 +0300

    slapd: engage biglock in the operations which are make changes.
    This is fixes issue:
      https://github.com/ReOpen/ReOpenLDAP/issues/12
    
    Change-Id: Idb70d239ea62316896deb9407cb92c15733d4aa8

--- servers/slapd/sasl.c
+++ servers/slapd/sasl.c
@@ -564,7 +564,7 @@ slap_auxprop_store(
 
 	op.o_bd = select_backend( &op.o_req_ndn, 1 );
 
-	if ( !op.o_bd || !op.o_bd->be_modify ) return SASL_FAIL;
+	if ( !op.o_bd || !op.o_bd->bd_info->bi_op_modify ) return SASL_FAIL;
 
 #ifdef SLAP_AUXPROP_DONTUSECOPY
 	if ( SLAP_SHADOW( op.o_bd ) && dontUseCopy ) {
@@ -624,7 +624,7 @@ slap_auxprop_store(
 
 			for (;;) {
 				SlapReply rs = {REP_RESULT};
-				rc = op.o_bd->be_modify( &op, &rs );
+				rc = slap_biglock_call_be( op_modify, &op, &rs );
 
 #ifdef SLAP_AUXPROP_DONTUSECOPY
 				if ( dontUseCopy &&
@@ -644,6 +644,7 @@ slap_auxprop_store(
 	slap_mods_free( modlist, 1 );
 	return rc != LDAP_SUCCESS ? SASL_FAIL : SASL_OK;
 }
+
 #endif /* SASL_VERSION_FULL >= 2.1.16 */
 
 static sasl_auxprop_plug_t slap_auxprop_plugin = {
