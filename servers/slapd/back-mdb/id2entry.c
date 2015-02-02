49cddfdc3a8a0829efcc9b31db7ea60453914aa3
Howard Chu <hyc@openldap.org>
2015-02-02 10:48:39 +0000

    slapd: ITS#8040 experimental Lazy Commit.
    For reasons discussed here
    http://www.openldap.org/lists/openldap-devel/201501/msg00014.html it would be
    good to implement this control. The behavior is documented here
    https://msdn.microsoft.com/en-us/library/cc223351.aspx
    
    The OID is listed here https://msdn.microsoft.com/en-us/library/cc223320.aspx as
    1.2.840.113556.1.4.619
    
    Change-Id: I47839e52af6df864ac44e2a243d8184ff7d13d1c

--- servers/slapd/back-mdb/id2entry.c
+++ servers/slapd/back-mdb/id2entry.c
@@ -466,7 +466,10 @@ mdb_opinfo_get( Operation *op, struct mdb_info *mdb, int rdonly, mdb_op_info **m
 				moi->moi_txn = mdb_tool_txn;
 			} else {
 				assert(slap_biglock_owned(op->o_bd));
-				rc = mdb_txn_begin( mdb->mi_dbenv, NULL, 0, &moi->moi_txn );
+				int flag = 0;
+				if ( get_lazyCommit( op ))
+					flag |= MDB_NOSYNC;
+				rc = mdb_txn_begin( mdb->mi_dbenv, NULL, flag, &moi->moi_txn );
 				if (rc) {
 					Debug( LDAP_DEBUG_ANY, "mdb_opinfo_get: err %s(%d)\n",
 						mdb_strerror(rc), rc );
